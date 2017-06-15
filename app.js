0401f219799fcb052abe5aa110103fa7a4640996
Carolina Knoll <carolinasknoll@gmail.com>
2017-06-15 18:34:39 -0300

    Rename run-all script and clean app.js file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const vash = require('vash');
 const app = express();
 
-var sass = require('node-sass');
+const vash = require('vash');
+const sass = require('node-sass');
 
 app.set('view engine', 'vash');
 app.set('port', process.env.PORT || 3000);
@@ -11,7 +11,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index.vash');
-  res.render(`${req.params.path}.scss`)
 })
 
 app.get('/:path', function(req, res) {
@@ -19,6 +18,6 @@ app.get('/:path', function(req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000.');
-  console.log('Open http://localhost:3000/ to start! :-)');
+  console.log('Listening on port 3000 (3001 if running with run-all)');
+  console.log('Open http://localhost:3000/ or http://localhost:3001/ to start! :-)');
 })
