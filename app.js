763f6f1492cb3603881476e7089a59851b35002e
Carolina Knoll <carolinasknoll@gmail.com>
2017-06-30 23:18:42 -0300

    Move routes to a new file

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const app = express();
 const vash = require('vash');
 const sass = require('node-sass');
 
+const search = require('./routes/search');
+
 app.set('view engine', 'vash');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.render('index.vash');
-})
-
-app.get('/:path', function(req, res) {
-  res.render(`${req.params.path}.vash`);
-})
+app.get('/', search.index);
 
 app.listen(3000, function () {
   console.log('Listening on port 3000 (3001 if running with run-all)');
